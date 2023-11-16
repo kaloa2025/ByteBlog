55a0b4eedc82b4a92d67aa144bc287777c74ab5d
kaloa2025 <aalokchoudhari2021@gmail.com>
2023-11-16 23:31:38 +0530

    Aalok-16/11/2023-23:31-Nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <table>
+            <tr>
+                <td>Logo</td>
+                <td></td>
+                <td>
+                    <table>
+                        <tr>
+                            <td>search</td>
+                            <td>profile</td>
+                            <td>trending</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </div>
+  )
+}
